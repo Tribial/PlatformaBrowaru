22ce39887c1dd5fc93b509663156e91f2229a660
Damian Jacyna <135932@student.uwm.edu.pl>
2018-03-17 17:09:07 +0100

    Models filled (BrandProduction, Brewery, BrewingGroup)

--- PlatformaBrowaru.WebApi/PlatformaBrowaru.Share/Models/Brewery.cs
+++ PlatformaBrowaru.WebApi/PlatformaBrowaru.Share/Models/Brewery.cs
@@ -6,6 +6,19 @@ namespace PlatformaBrowaru.Share.Models
 {
     public class Brewery //Damian Jacyna
     {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Description { get; set; }
+        public decimal AnnualProduction { get; set; }
+        public DateTime CreationDate { get; set; }
+        public virtual BrewingGroup Owner { get; set; }
+        public virtual ApplicationUser AddedBy { get; set; }
+        public DateTime AddedAt { get; set; }
+        public string DeletionReason { get; set; }
+        public DateTime EditedAt { get; set; }
+        public virtual ApplicationUser EditedBy { get; set; }
 
+        public virtual BrewingGroup BrewingGroup { get; set; }
+        public virtual List<BrandProduction> BrandProductions { get; set; }
     }
 }
